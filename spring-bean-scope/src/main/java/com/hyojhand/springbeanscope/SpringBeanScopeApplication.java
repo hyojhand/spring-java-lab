f4b0b1617a22e773a4d570b1be6531ae8cbc342f
손효재 <sonjw5128@gmail.com>
2023-04-01 21:49:38 +0900

    init: spring bean scope project

---
+++ spring-bean-scope/src/main/java/com/hyojhand/springbeanscope/SpringBeanScopeApplication.java
@@ -0,0 +1,13 @@
+package com.hyojhand.springbeanscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanScopeApplication.class, args);
+	}
+
+}
